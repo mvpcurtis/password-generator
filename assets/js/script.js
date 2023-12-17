4b13281f8d20b530c9c3a0131e169d9dd0371cf9
mvpcurtis <curtis.bowen@gmx.com>
2023-12-17 10:48:13 +0000

    created the logic for the password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,25 +10,48 @@ var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Function to prompt user for password options
-function getPasswordOptions() {}
-
-// Function for getting a random element from an array
-function getRandom(arr) {}
-
 // Function to generate password with user input
-function generatePassword() {}
+function generatePassword() {
+	// MIN & MAX CHRACTER LENGTH
+	const minLength = 8;
+	const maxLength = 128;
+	// CHRACTER LENGTH PROMPT
+	let length = prompt(`Please choose how many chracters you'd like your password to contain (between ${minLength} and ${maxLength}).`);
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+	length = parseInt(length);
+	if (isNaN(length) || length < minLength || length > maxLength) {
+		alert(`Please enter a valid password between ${minLength} and ${maxLength}.`);
+		return;
+	}
 
-// Write password to the #password input
-function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
+	// INITIALISE CHARACTERSET
+	let characterSet = lowerCasedCharacters.slice(); //CREATES A COPY OF THE LOWERCASECHARACTERS ARRAY
+	// PROMPT USER TO SEE IF THEY'D LIKE TO USE UPPERCASE CHARACTERS IN THEIR PASSWORD
+	const useUppercase = confirm("Would you like to include uppercase chracters within your password?");
+	// PROMPT USER TO SEE IF THEY'D LIKE TO USE LOWERCASE CHARACTERS IN THEIR PASSWORD
+	const useNumbers = confirm("Would you like to include numbers within your password?");
+	// PROMPT USER TO SEE IF THEY'D LIKE TO USE SPECIAL CHARACTERS IN THEIR PASSWORD
+	const useSpecial = confirm("Would you like to include special characters within your password?");
 
-	passwordText.value = password;
+	// CHECKS IF THE USER HAS CONFIRMED TO INCLUDE UPPERCASE CHARACTERS
+	if (useUppercase) {
+		characterSet = characterSet.concat(upperCasedCharacters);
+	}
+	// CHECKS IF THE USER HAS CONFIRMED TO INCLUDE NUMERIC VALUES
+	if (useNumbers) {
+		characterSet = characterSet.concat(numericCharacters);
+	}
+	// CHECKS IF THE USER HAS CONFIRMED TO INCLUDE SPEICAL CHARACTERS
+	if (useSpecial) {
+		characterSet = characterSet.concat(specialCharacters);
+	}
+	// INITIALISE EMPTY PASSWORD VARIABLE
+	let password = "";
+	// FOR LOOP
+	for (i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characterSet.length);
+		password += characterSet[randomIndex];
+	}
+
+	document.getElementById("password").textContent = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
